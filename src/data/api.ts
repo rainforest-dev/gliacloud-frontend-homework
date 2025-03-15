43daf428a4e64ff6bdeb7e060244cee638452a22
Rainforest Cheng <contact@rainforest.tools>
2025-03-15 02:57:04 +0000

    refactor: modify subtitlesFetcher to remove file parameter and adjust formatting

--- src/data/api.ts
+++ src/data/api.ts
@@ -1,11 +1,13 @@
 import type { ISectionSubtitles } from "@/types";
 
-export const subtitlesFetcher = async (file: File) => {
-  const formData = new FormData();
-  formData.append("file", file);
-  const response = await fetch("/api/video/analyze", {
-    method: "POST",
-    body: formData,
-  });
-  return response.json() as Promise<ISectionSubtitles[]>;
-};
+export const subtitlesFetcher = async () =>
+  // file: File
+  {
+    const formData = new FormData();
+    // formData.append("file", file);
+    const response = await fetch("/api/video/analyze", {
+      method: "POST",
+      body: formData,
+    });
+    return response.json() as Promise<ISectionSubtitles[]>;
+  };
